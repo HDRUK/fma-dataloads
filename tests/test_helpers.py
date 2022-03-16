435aa012eaff14e5f9d8ee686b0de3780962c6f4
Callum Reekie <cdjreekie@gmail.com>
2022-03-16 20:20:57 +0000

    more helper tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,11 @@
 from functions.helpers import *
 
-custodian_datasets = [{"identifier": "xyz"}, {"identifier": "abc"}, {"identifier": "pqr"}, {"identifier": "mno"}]
+custodian_datasets = [
+    {"identifier": "xyz"},
+    {"identifier": "abc"},
+    {"identifier": "pqr"},
+    {"identifier": "mno"},
+]
 gateway_datasets = [{"pid": "xyz"}, {"pid": "abc"}, {"pid": "pqr"}, {"pid": "def"}]
 
 
@@ -8,7 +13,7 @@ def test_datasets_to_archive():
     """
     Function should identifify correct datasets to archive in the Gateway.
     """
-    datasets = datasets_to_archive(custodian_datasets=custodian_datasets, gateway_datasets=gateway_datasets)
+    datasets = datasets_to_archive(custodian_datasets, gateway_datasets)
 
     assert len(datasets) == 1
     assert datasets[0] == {"pid": "def"}
@@ -18,7 +23,25 @@ def test_datasets_to_add():
     """
     Function should identifify correct datasets that are new to the Gateway.
     """
-    datasets = extract_new_datasets(custodian_datasets=custodian_datasets, gateway_datasets=gateway_datasets)
+    datasets = extract_new_datasets(custodian_datasets, gateway_datasets)
 
     assert len(datasets) == 1
     assert datasets[0] == {"identifier": "mno"}
+
+
+def test_extract_overlapping_datasets():
+    datasets_1, datasets_2 = extract_overlapping_datasets(
+        custodian_datasets, gateway_datasets
+    )
+
+    expected_datasets_1 = [
+        {"identifier": "xyz"},
+        {"identifier": "abc"},
+        {"identifier": "pqr"},
+    ]
+    expected_datasets_2 = [{"pid": "xyz"}, {"pid": "abc"}, {"pid": "pqr"}]
+
+    assert len(datasets_1) == 3
+    assert len(datasets_2) == 3
+    assert [a == b for a, b in zip(datasets_1, expected_datasets_1)]
+    assert [a == b for a, b in zip(datasets_2, expected_datasets_2)]
