200aa90f742fd09e88d3281ac5d0f1d0aed8356f
Dan Nita <dan.nita@hdruk.ac.uk>
2023-11-08 12:55:05 +0000

    update - errors

--- main.py
+++ main.py
@@ -322,18 +322,18 @@ def main(custodian_id: str) -> None:
         # Emails
         ##########################################
         try:
-            logging.debug(json.dumps(datasets, ensure_ascii=True).encode("ascii", "replace"))
-            logging.debug(json.dumps(archived_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            logging.debug(json.dumps(new_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            logging.debug(json.dumps(updated_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            logging.debug(json.dumps(invalid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            logging.debug(json.dumps(unsupported_version_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            datasets = json.loads(json.dumps(datasets, ensure_ascii=True).encode("ascii", "replace"))
-            archived_datasets = json.loads(json.dumps(archived_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            new_valid_datasets = json.loads(json.dumps(new_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            updated_valid_datasets = json.loads(json.dumps(updated_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            invalid_datasets = json.loads(json.dumps(invalid_datasets, ensure_ascii=True).encode("ascii", "replace"))
-            unsupported_version_datasets = json.loads(json.dumps(unsupported_version_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(archived_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(new_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(updated_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(invalid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # logging.debug(json.dumps(unsupported_version_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # datasets = json.loads(json.dumps(datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # archived_datasets = json.loads(json.dumps(archived_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # new_valid_datasets = json.loads(json.dumps(new_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # updated_valid_datasets = json.loads(json.dumps(updated_valid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # invalid_datasets = json.loads(json.dumps(invalid_datasets, ensure_ascii=True).encode("ascii", "replace"))
+            # unsupported_version_datasets = json.loads(json.dumps(unsupported_version_datasets, ensure_ascii=True).encode("ascii", "replace"))
             if any(
                 len(datasets) > 0
                 for datasets in [
